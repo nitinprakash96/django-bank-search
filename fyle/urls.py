905af0e2ab854cdc2eea9bc3a2e90ceaa3eba741
Nitin Prakash <prakash.nitin63@gmail.com>
2019-10-20 20:18:26 +0530

    URL patterns for making requests

--- fyle/urls.py
+++ fyle/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import permissions
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('rest_registration.api.urls')),
+    path('bank/', include('banks.urls')),
+    path('api-token-auth/', obtain_jwt_token),
+    path('api-token-refresh/', refresh_jwt_token)
 ]
